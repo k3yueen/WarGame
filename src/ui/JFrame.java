baff9d3c7a0847a9fa14f1c18288c8188ea878a6
Roza Antonevici <rozic.antonevici@me.com>
2025-06-24 01:25:06 +0200

    Update JFrame.java
    Added comments to each line of code for short and simple explanation of what each line does so that you can all understand

--- src/ui/JFrame.java
+++ src/ui/JFrame.java
@@ -2,22 +2,26 @@ package ui;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
-    private GamePanel gamePanel;
-    
-    public GameFrame(GameController controller) {
-        super("Card Game");
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        add(gamePanel = new GamePanel(controller));
-        setJMenuBar(new MenuBar(controller));
-        setVisible(true);
-    }
-    
-    public GamePanel getGamePanel() { return gamePanel; }
-    public void setGamePanel(GamePanel gamePanel) { this.gamePanel = gamePanel; }
-    
-    public void printInfo() {
-        System.out.println("Game Frame Info:\nTitle: " + getTitle() + "\nSize: " + getSize());
-    }
+   private GamePanel gamePanel;
+   
+   //Creates the main game window
+   public GameFrame(GameController controller) {
+       super("Card Game"); //Sets window title
+       setExtendedState(JFrame.MAXIMIZED_BOTH); //Extends the background image to fullscreen
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exit when closed
+       
+       add(gamePanel = new GamePanel(controller)); //Adds game area
+       setJMenuBar(new MenuBar(controller)); //Adds menu bar
+       setVisible(true); //Shows window
+   }
+   
+   //Gets the game panel
+   public GamePanel getGamePanel() { return gamePanel; }
+   //Sets the game panel
+   public void setGamePanel(GamePanel gamePanel) { this.gamePanel = gamePanel; }
+   
+   //Prints window info for debugging
+   public void printInfo() {
+       System.out.println("Game Frame Info:\nTitle: " + getTitle() + "\nSize: " + getSize());
+   }
 }
