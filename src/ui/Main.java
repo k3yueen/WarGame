160dc25585ed2c45de2d33a251cb93a7739dfe04
Roza Antonevici <rozic.antonevici@me.com>
2025-06-29 19:24:27 +0200

    Update and rename RealMain.java to Main.java
    Commented last version of Main.java

---
+++ src/ui/Main.java
@@ -0,0 +1,11 @@
+//Ademola Emmanuel Adegbola
+package ui;
+
+//Create the Main class which is the entry point of the application. 
+public class Main {
+  public static void main(String[] args) {
+    //Launch the landing page instead of directly starting the game
+    LandingPage landingPage = new LandingPage();
+    landingPage.setVisible(true);
+  }
+}
